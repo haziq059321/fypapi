d4a2a6e7787b4abc3a3f8d399ea247682503225e
haziq059321 <haziqshiekh824@gmail.com>
2024-02-24 01:47:24 +0500

    update HydroData.js

--- controllers/HydroData.js
+++ controllers/HydroData.js
@@ -6,10 +6,10 @@ const dataapi = async (req, res) => {
     res.status(200).json(myData);
 };
 
-<<<<<<< HEAD
+
 
 module.exports = { dataapi };
-=======
-module.exports =dataapi;
->>>>>>> da96f57b8d46e3a26242e4249a716c57de617bba
+
+module.exports = dataapi;
+
 
