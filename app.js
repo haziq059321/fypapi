1e3af5dcabde88f9ac4394ee3cdcefb64e324d47
haziq059321 <haziqshiekh824@gmail.com>
2024-02-23 23:16:13 +0500

    modified

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const r1 = require('./routes/data')
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/api/data", (req, res) => {
+app.get("/", (req, res) => {
     res.send("hey");
 })
 app.use("/api/data", r1)
