831c4d8c9408a2a25aaa63220d756023159f62c0
haziq059321 <haziqshiekh824@gmail.com>
2024-02-23 23:15:39 +0500

    modified

--- controllers/data.js
+++ controllers/data.js
@@ -5,9 +5,9 @@ const dataapi = async (req, res) => {
     const myData = await datta.find({});
     res.status(200).json({ myData });
 };
-// //const dataapitesting = async (req, res) => {
-//     res.status(200).json({ msg: "this is our Hydroponics Data" });
-// };
+const dataapitesting = async (req, res) => {
+    res.status(200).json({ msg: "this is our Hydroponics Data" });
+};
 
 module.exports = { dataapi, dataapitesting };
 
