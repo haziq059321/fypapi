cd1d37fbc086080e92007e899b28ac4e6d4931f4
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2024-02-23 21:23:11 +0100

    added mongodb connection string

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const data = require('./data2')
 const mqtt1 = require('./models/mqtt')
 const r1 = require('./routes/data')
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 app.use(cors({ origin: '*' }));
@@ -14,8 +17,16 @@ app.use("/api/data", r1)
 app.use('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(8080, () => {
-    console.log(`yes app is running`);
-});
+
+mongoose
+  .connect(process.env.MONGOODB_URL)
+  .then((result) => {
+    app.listen(4000, () => {
+      console.log('listening on port 4000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = app;
