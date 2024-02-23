53d72b1179902506acee0d46c4afbeed06dd4f83
haziq059321 <haziqshiekh824@gmail.com>
2024-02-24 01:43:47 +0500

    update HydroData.js

--- controllers/HydroData.js
+++ controllers/HydroData.js
@@ -5,9 +5,7 @@ const dataapi = async (req, res) => {
     const myData = await datta.find({});
     res.status(200).json(myData);
 };
-const dataapitesting = async (req, res) => {
-    res.status(200).json({ msg: "this is our Hydroponics Data" });
-};
 
-module.exports = { dataapi, dataapitesting };
+
+module.exports = { dataapi };
 
