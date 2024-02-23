d4d54a2db35d5b4d1ce007e3f4d2b9304c66929f
Afham <56600934+SheikhAfham-1993@users.noreply.github.com>
2024-02-23 20:50:57 +0100

    Refactored the code and put the default route to the end

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 require("dotenv").config();
 const express = require('express')
-const app = express();
-app.use(cors({ origin: '*' }));
 
-// parse application/json
-app.use(express.json());
 const data = require('./data2')
 const mqtt1 = require('./models/mqtt')
 const r1 = require('./routes/data')
 
-//const connectdb = require('./db/connect')
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("hey");
-})
+const app = express();
+app.use(cors({ origin: '*' }));
+// parse application/json
+app.use(express.json());
+
+
 app.use("/api/data", r1)
+app.use('/', (req, res) => {
+  res.send('Hello World!');
+});
 const start = async () => {
     try {
-        //await dbconnect();
         app.listen(PORT, () => {
             console.log(`${PORT} yes app is running`);
         });
