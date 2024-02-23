88cb9a30715a00f6bc2a0db7eb67a089f9441a56
haziq059321 <haziqshiekh824@gmail.com>
2024-02-23 23:19:36 +0500

    file name changed

---
+++ controllers/HydroData.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const datta = require('../models/mqtt')
+
+const dataapi = async (req, res) => {
+    const myData = await datta.find({});
+    res.status(200).json(myData);
+};
+const dataapitesting = async (req, res) => {
+    res.status(200).json({ msg: "this is our Hydroponics Data" });
+};
+
+module.exports = { dataapi, dataapitesting };
+
