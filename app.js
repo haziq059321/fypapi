0ad829504a950caf302363fd05e401044ccfd50e
haziq059321 <haziqshiekh824@gmail.com>
2024-02-24 01:42:40 +0500

    refactor: removed unused import

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-require("dotenv").config();
 const express = require('express')
+const cors = require('cors');
+const r1 = require('./routes/data')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
 const app = express();
 app.use(cors({ origin: '*' }));
-
 // parse application/json
 app.use(express.json());
-const data = require('./data2')
-const mqtt1 = require('./models/mqtt')
-const r1 = require('./routes/data')
 
-//const connectdb = require('./db/connect')
 
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("hey");
-})
+
+
+
 app.use("/api/data", r1)
-const start = async () => {
-    try {
-        //await dbconnect();
-        app.listen(PORT, () => {
-            console.log(`${PORT} yes app is running`);
-        });
 
-    } catch (error) {
-        console.log(error);
-    }
-}
-start();
+app.use("/", (req, res) => {
+    res.send("hello world");
+})
+
+
+mongoose
+    .connect(process.env.MONGOODB_URL)
+    .then(() => {
+        app.listen(4000, () => {
+            console.log('listening on port 4000');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 module.exports = app;
